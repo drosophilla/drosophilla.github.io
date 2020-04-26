1d4af1de25e3698fe5b1f5d7d5805741c2667664
drosophilla <aigokhu@rocketmail.com>
2020-04-26 13:56:10 +0800

    1

---
+++ data/TDTORDE3LU_3.js
@@ -0,0 +1,27 @@
+var json_TDTORDE3LU_3 = {
+"type": "FeatureCollection",
+"name": "TDTORDE3LU_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "no": "19.08.011", "x": 121.40949, "y": 1.16777, "X_TM3": null, "Y_TM3": null }, "geometry": { "type": "Point", "coordinates": [ 121.40948970277779, 1.167769711111111 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.012", "x": 121.428076, "y": 1.166315, "X_TM3": null, "Y_TM3": null }, "geometry": { "type": "Point", "coordinates": [ 121.428076438888894, 1.166314588888889 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.013", "x": 121.435248, "y": 1.159754, "X_TM3": null, "Y_TM3": null }, "geometry": { "type": "Point", "coordinates": [ 121.435247744444453, 1.159754108333333 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.014", "x": 121.412422, "y": 1.15412, "X_TM3": null, "Y_TM3": null }, "geometry": { "type": "Point", "coordinates": [ 121.41242170000001, 1.154120147222222 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.015", "x": 121.365465, "y": 1.112541, "X_TM3": 185027.972733, "Y_TM3": 1623006.6367899999 }, "geometry": { "type": "Point", "coordinates": [ 121.365465472222212, 1.112541241666666 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.016", "x": 121.413304, "y": 1.124893, "X_TM3": 190351.81736, "Y_TM3": 1624372.14117 }, "geometry": { "type": "Point", "coordinates": [ 121.41330366944446, 1.124893425 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.017", "x": 121.455857, "y": 1.134711, "X_TM3": null, "Y_TM3": null }, "geometry": { "type": "Point", "coordinates": [ 121.45585725, 1.134711186111111 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.018", "x": 121.467894, "y": 1.124282, "X_TM3": 196426.98146800001, "Y_TM3": 1624304.4244299999 }, "geometry": { "type": "Point", "coordinates": [ 121.467893680555562, 1.124282075 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.019", "x": 121.457766, "y": 1.112653, "X_TM3": 195299.89144899999, "Y_TM3": 1623018.63763 }, "geometry": { "type": "Point", "coordinates": [ 121.457766077777791, 1.112652566666666 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.020", "x": 121.427601, "y": 1.10449, "X_TM3": 191942.88233200001, "Y_TM3": 1622116.1886799999 }, "geometry": { "type": "Point", "coordinates": [ 121.4276011, 1.104489725 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.021", "x": 121.414452, "y": 1.098886, "X_TM3": 190479.50409199999, "Y_TM3": 1621496.6752800001 }, "geometry": { "type": "Point", "coordinates": [ 121.414451780555567, 1.09888615 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.022", "x": 121.376084, "y": 1.096431, "X_TM3": 186209.57120499999, "Y_TM3": 1621225.35776 }, "geometry": { "type": "Point", "coordinates": [ 121.376083641666668, 1.096430852777778 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.023", "x": 121.336015, "y": 1.093652, "X_TM3": 181750.39415599999, "Y_TM3": 1620918.3557200001 }, "geometry": { "type": "Point", "coordinates": [ 121.336015158333325, 1.093652225 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.024", "x": 121.43518, "y": 1.066695, "X_TM3": 192786.27866899999, "Y_TM3": 1617937.4471700001 }, "geometry": { "type": "Point", "coordinates": [ 121.435180413888887, 1.066695019444445 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.025", "x": 121.448365, "y": 1.07893, "X_TM3": 194253.64137600001, "Y_TM3": 1619290.1199399999 }, "geometry": { "type": "Point", "coordinates": [ 121.448365333333356, 1.078929605555555 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.026", "x": 121.457609, "y": 1.093689, "X_TM3": 195282.39129299999, "Y_TM3": 1620921.95101 }, "geometry": { "type": "Point", "coordinates": [ 121.45760909444445, 1.09368895 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.027", "x": 121.489375, "y": 1.103514, "X_TM3": 198817.533169, "Y_TM3": 1622008.1881 }, "geometry": { "type": "Point", "coordinates": [ 121.4893747, 1.103513775 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.028", "x": 121.478412, "y": 1.087772, "X_TM3": 197597.49565, "Y_TM3": 1620267.72749 }, "geometry": { "type": "Point", "coordinates": [ 121.478411913888905, 1.087772005555555 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.029", "x": 121.458833, "y": 1.05632, "X_TM3": 195418.54962899999, "Y_TM3": 1616790.2510800001 }, "geometry": { "type": "Point", "coordinates": [ 121.458833069444452, 1.056319538888888 ] } },
+{ "type": "Feature", "properties": { "no": "19.08.030", "x": 121.455047, "y": 1.039454, "X_TM3": 194997.15787699999, "Y_TM3": 1614925.524 }, "geometry": { "type": "Point", "coordinates": [ 121.455046866666649, 1.039453838888889 ] } }
+]
+}
